f5d2df2025f867bfe901f7bb92d473959ed2135f
Ng Yin Kit <yinkit.ng.2021@scis.smu.edu.sg>
2022-10-01 17:10:46 +0800

    wk 7 update

---
+++ mod.js
@@ -0,0 +1,15 @@
+function process_input() {
+
+    let answer = document.getElementById("ans").value.toUpperCase()
+    let result = document.getElementById('result')
+    if(answer=='IS216'){
+        result.style.color = "green"
+        result.innerText="Congratulations! you've won the game!"
+        alert("You're free to go!")
+    }
+    else{
+        result.style.color = "red"
+        result.innerText="Sorry, try again."
+        alert("Continue coding CSS and JS in the dark room all night!")
+    }
+}
