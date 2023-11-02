a0c5244e5eb29453c95f3253803ea9031989ff1f
ngyinkit <111484636+ngyinkit@users.noreply.github.com>
2023-11-02 15:54:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const app = Vue.createApp({
                 {name: 'SPIRITCYBER-22', link: 'https://wis.ntu.edu.sg/webexe88/owa/REGISTER_NTU.REGISTER?EVENT_ID=OA22100610102757', rank:'3 / 13', writeup:'https://medium.com/@yoongsofties/spiritcyber-22-writeup-589f9e171b8f'},
                 {name: 'Grey Cat The Flag 2023', link: 'https://ctf.nusgreyhats.org/', rank:'140 / 454', writeup:'https://medium.com/@yoongsofties/grey-cat-the-flag-2023-writeup-ea13830ac805'},
                 {name: 'SEETF 2023', link: 'https://seetf.sg/seetf/about', rank:'138 / 431', writeup:'https://medium.com/@yoongsofties/seetf-2023-writeup-769de53f19ba'},
+                {name: 'TISC 2023', link: 'https://www.csit.gov.sg/events/tisc/tisc-2023', rank:'-', writeup:'https://chiseled-part-43f.notion.site/TISC-2023-439050d10f114ede84db9f1ac488b07f'},
+                {name: 'ASCIS 2023', link: 'https://ascis.vnisa.org.vn/en/overview/', rank:'-', writeup:'https://chiseled-part-43f.notion.site/ASCIS-CTF-2023-11ccc140c70b4288a8f6ed432c316f30'},
+                // {name: '', link: '', rank:'', writeup:''},
             ],
 
             hackathons: [
