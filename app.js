a95e996b1122f80a5dfc0802b9623521dd4d665a
ngyinkit <111484636+ngyinkit@users.noreply.github.com>
2023-12-06 13:32:10 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const app = Vue.createApp({
                 {topic: 'Cybersecurity', source: 'resources/pico-icon.png', sitename: 'PicoCTF', link: 'https://picoctf.org/', text: 'Learning resource and platform to practice for CTFs. I used this to prepare for my first CTF in 2022.'},
                 {topic: 'Cybersecurity', source: 'resources/htb-icon.png', sitename: 'HackTheBox', link: 'https://www.hackthebox.com/', text: 'Another platform to practice for CTFs. I subsequently also started using this platform later in the year.'},
                 {topic: 'Cheatsheet', source: 'resources/overapi-icon.png', sitename: 'OverAPI', link: 'https://overapi.com/', text: 'One-stop platform for coding cheatsheets. Personally don\'t necessarily find all of them useful, though.'},
-                {topic: 'Korean', source: 'resources/shk-icon.png', sitename: 'SayHiKorean', link: 'https://sayhikorean.blogspot.com/', text: 'Fun fact: I taught myself Korean since I was in primary school. This is my favourite resource for learning Korean grammar.'}
             ],
 
             ctfs: [
@@ -16,6 +15,7 @@ const app = Vue.createApp({
                 {name: 'SEETF 2023', link: 'https://seetf.sg/seetf/about', rank:'138 / 431', writeup:'https://medium.com/@yoongsofties/seetf-2023-writeup-769de53f19ba'},
                 {name: 'TISC 2023', link: 'https://www.csit.gov.sg/events/tisc/tisc-2023', rank:'-', writeup:'https://chiseled-part-43f.notion.site/TISC-2023-439050d10f114ede84db9f1ac488b07f'},
                 {name: 'ASCIS 2023', link: 'https://ascis.vnisa.org.vn/en/overview/', rank:'-', writeup:'https://chiseled-part-43f.notion.site/ASCIS-CTF-2023-11ccc140c70b4288a8f6ed432c316f30'},
+                {name: 'Advent of Cyber 2023', link: 'https://tryhackme.com/r/christmas', rank:'-', writeup:'https://chiseled-part-43f.notion.site/TryHackMe-Advent-of-Cyber-2023-f2c851f8cad7420cb9df2c7b6679e151'},
                 // {name: '', link: '', rank:'', writeup:''},
             ],
 
