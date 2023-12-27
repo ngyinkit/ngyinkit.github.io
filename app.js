4887526b0ba9d6ed08ddcfe27d2405a840e542d4
ngyinkit <111484636+ngyinkit@users.noreply.github.com>
2023-12-27 15:54:06 +0800

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const app = Vue.createApp({
                 {name: 'SEETF 2023', link: 'https://seetf.sg/seetf/about', rank:'138 / 431', writeup:'https://medium.com/@yoongsofties/seetf-2023-writeup-769de53f19ba'},
                 {name: 'TISC 2023', link: 'https://www.csit.gov.sg/events/tisc/tisc-2023', rank:'-', writeup:'https://chiseled-part-43f.notion.site/TISC-2023-439050d10f114ede84db9f1ac488b07f'},
                 {name: 'ASCIS 2023', link: 'https://ascis.vnisa.org.vn/en/overview/', rank:'-', writeup:'https://chiseled-part-43f.notion.site/ASCIS-CTF-2023-11ccc140c70b4288a8f6ed432c316f30'},
-                {name: 'Advent of Cyber 2023', link: 'https://tryhackme.com/r/christmas', rank:'-', writeup:'https://chiseled-part-43f.notion.site/TryHackMe-Advent-of-Cyber-2023-f2c851f8cad7420cb9df2c7b6679e151'},
                 // {name: '', link: '', rank:'', writeup:''},
             ],
 
