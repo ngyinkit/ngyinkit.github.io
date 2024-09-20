f502ff6fdfc2690839e23931982296b1531d9c05
ngyinkit <111484636+ngyinkit@users.noreply.github.com>
2024-09-20 23:24:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = Vue.createApp({
                 {name: 'SEETF 2023', link: 'https://seetf.sg/seetf/about', rank:'138 / 431', writeup:'https://medium.com/@yoongsofties/seetf-2023-writeup-769de53f19ba'},
                 {name: 'TISC 2023', link: 'https://www.csit.gov.sg/events/tisc/tisc-2023', rank:'N/A', writeup:'https://chiseled-part-43f.notion.site/TISC-2023-439050d10f114ede84db9f1ac488b07f'},
                 {name: 'ASCIS 2023', link: 'https://ascis.vnisa.org.vn/en/overview/', rank:'unknown', writeup:'https://chiseled-part-43f.notion.site/ASCIS-CTF-2023-11ccc140c70b4288a8f6ed432c316f30'},
-                {name: 'CDDC 2024 (qualifiers)', link: 'https://www.dstabrainhack.com/activities/cyber-defenders-discovery-camp', rank:'75', writeup:'https://chiseled-part-43f.notion.site/CDDC-2024-qualifiers-37013e1a08554141a56735300874eec0'},
+                {name: 'CDDC 2024 (qualifiers)', link: 'https://www.dstabrainhack.com/activities/cyber-defenders-discovery-camp', rank:'75 (Uni)', writeup:'https://chiseled-part-43f.notion.site/CDDC-2024-qualifiers-37013e1a08554141a56735300874eec0'},
                 {name: 'CISS 2024', link: 'https://itrust.sutd.edu.sg/ciss/', rank:'23 / 50', writeup:'https://chiseled-part-43f.notion.site/CISS-2024-1013de7545088043b210c79a0475b62b'},
                 // {name: '', link: '', rank:'', writeup:''},
             ],
